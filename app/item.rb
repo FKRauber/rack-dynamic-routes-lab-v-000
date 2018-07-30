fffb24ea1a69c5021404c0f9cf2c9890fa65c388
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-07-30 02:45:12 +0000

    automatically backed up by learn

--- app/item.rb
+++ app/item.rb
@@ -5,16 +5,4 @@ class Item
     @name = name
     @price = price
   end
-
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
-
-    if req.path=="/items/self.name"
-        resp.write "#{self.price}"
-    else
-      resp.write "Route not found"
-      resp.status = 404
-    end
-  end
 end
