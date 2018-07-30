6493b4eeab8802fc78b3ab99e9325d102bdd1a9f
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-07-30 02:48:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,8 @@ class application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item = @@items.find{|item| item.name == item_name}
         resp.write "#{self.price}"
     else
       resp.write "Route not found"
